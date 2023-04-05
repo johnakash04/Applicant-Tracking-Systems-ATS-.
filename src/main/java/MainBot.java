ac5dc0b2bff3f9e8b396de0829f783038d14b153
Lucas Hunter <lucasbhunter@gmail.com>
2023-04-05 17:07:34 -0300

    small fix

--- src/main/java/MainBot.java
+++ src/main/java/MainBot.java
@@ -1,6 +1,6 @@
 public class MainBot {
     public static void main(String[] args){
-        ConnectBot bot = new ConnectBot();
+        new ConnectBot();
         System.out.println("Bot has connected...");
     }
 }
